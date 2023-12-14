0832fa90be583432c44a8e32e6dd1029982571b8
abdiqani warsame <abdiqanimw@gmail.com>
2023-12-14 12:54:36 +0000

    I created the functio that i inizialed in the event listene inside it i created a const variable name of id and a value of e.target.id which essentially grads the id of the element that was clicked

--- asset/JavaScript/interactivity.js
+++ asset/JavaScript/interactivity.js
@@ -11,4 +11,6 @@ let currentPlayer = X_TEXT;
 let spaces = Array(9).fill(null)// This line of code creates an array named spaces with a length of 9, representing the tictactoe grid the fill (null) method is used to initialize all the elements in this array with the value null.
 const suiii = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked));
-}
+} 
+function boxClicked(e) { // the parameter is representing the  event but can also be written in any way like evt,et and etc as a parameter but i just chose to put e.
+    const id = e.target.id //e: It's a parameter representing an event, like a click, e.target: This refers to the element that triggered the event, in this case, the specific box that was clicked e.target.id: The id property retrieves the unique identifier (ID) of the clicked element. So, const id = e.target.id; is essentially grabbing the ID of the element that was clicked.
